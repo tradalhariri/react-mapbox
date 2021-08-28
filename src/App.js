1ea8fbb5d1d80f57950d29577732331410907ee5
tradalhariri <trad.alhariri@gmail.com>
2021-08-28 23:38:19 +0300

    first

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import './App.css';
+import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import MapboxDirections from '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions'
+import '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css'
+
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_KEY;
+
+class App extends React.Component {
+
+  componentDidMount() {
+
+    // Creates new map instance
+    const map = new mapboxgl.Map({
+      container: this.mapWrapper,
+      style: 'mapbox://styles/mapbox/streets-v10',
+      center: [-73.985664, 40.748514],
+      zoom: 12
+    });
+
+    // Creates new directions control instance
+    const directions = new MapboxDirections({
+      accessToken: mapboxgl.accessToken,
+      unit: 'metric',
+      profile: 'mapbox/driving',
+    });
+
+    // Integrates directions control with map
+    map.addControl(directions, 'top-left');
+    map.addControl(new mapboxgl.GeolocateControl({
+      positionOptions: {
+      enableHighAccuracy: true
+      },
+      trackUserLocation: true,
+      showUserHeading: true,
+      showAccuracyCircle:true
+
+      }));
+  }
+
+  render() {
+    return (
+      // Populates map by referencing map's container property
+      <div ref={el => (this.mapWrapper = el)} className="mapWrapper" />
+    );
+  }
+}
+
+export default App;
